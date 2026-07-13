f3454fe09efeec4aae5f90d9ae3c8ea6062f13e9
Muazzam Ali <muazzam8845@gmail.com>
2026-07-13 19:01:05 +0500

    Third commit

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app=FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"message":"Hello,World!"}
+def load_data():
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    return data
 
+@app.get("/")
+def hello():
+    return {"message": "PATIENT management system api!"}
+
+@app.get("/about")
+def about():
+    return {"message": "This is a patient management system API built with FastAPI."}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
