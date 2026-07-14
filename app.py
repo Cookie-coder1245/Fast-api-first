e3a8a6ba0515158ae685451cf4901c5faf7c921b
Muazzam Ali <muazzam8845@gmail.com>
2026-07-14 17:06:59 +0500

    Commit 5

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI,Path,HTTPException,Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel,computed_field
+from typing import Annotated,Field,Literal
 import json
 
 app=FastAPI()
+
+class Patient(BaseModel):
+    id:Annotated[str,Field(...,description="Id of the patient",example=['1'])]
+    name:Annotated[str,Field(...,description="Name of the patient")]
+    city:Annotated[str,Field(...,description="City of the patient")]
+    age:Annotated[int,Field(...,gt=0,lt=0,description="City of the patient")]
+    gender:Annotated[Literal['male','female','other'],Field(...,description="age of the gender")]
+    height:Annotated[float,Field(...,gt=0,description="Height of the patient in meters")]
+    weight:Annotated[float,Field(...,gt=0,description="Weight of the patient in meters")]
+
+    @computed_field
+    @property
+    def bmi(self)->float:
+        bmi=round(self.weight/(self.height**2),2)
+        return bmi
+
 
 def load_data():
     with open("patient.json", "r") as file:
         data = json.load(file)
     return data
+def save_data(data):
+    with open('patient.json','w') as f:
+        json.dump(data,f)
 
 @app.get("/")
 def hello():
@@ -49,3 +71,22 @@ def sort_patient(sort_by:str=Query(...,description="sort on the basis of height,
     sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
 
     return sorted_data
+
+
+@app.post('/create')
+def create_patient(patient:Patient):
+
+    #load existing data
+    data=load_data()
+
+    #check if the pateint already exist
+    if patient.id in data:
+        raise HTTPException(status_code=400,detail="Patient already exists")
+
+    #new patient add to the database
+    data[patient.id]=patient.model_dump(exclude=['id'])
+    save_data(data)
+    
+    return JSONResponse(status_code=201,content={'message':'patient created successfully'})
+    
+
